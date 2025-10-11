3903faf492cc96a3574205b40e6d969c9e2c0e39
sandsmaster <chichkoliobopitko@gmail.com>
2025-10-11 18:36:02 +0200

    Set Framerate

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -3,6 +3,8 @@ import constants
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
@@ -14,6 +16,7 @@ def main():
                 return
         pygame.Surface.fill(screen,"black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":

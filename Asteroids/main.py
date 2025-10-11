36166bb26a27cd10ce9c57d98ac03dcc4725b310
sandsmaster <chichkoliobopitko@gmail.com>
2025-10-11 17:50:53 +0200

    Create game window

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -2,9 +2,18 @@ import pygame
 import constants
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,"black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":

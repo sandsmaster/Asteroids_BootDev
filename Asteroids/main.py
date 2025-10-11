ef33d71d30355e9885dac80d301f8850cefd5d62
sandsmaster <chichkoliobopitko@gmail.com>
2025-10-11 19:59:29 +0200

    Draw Player on Screen

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,11 +1,15 @@
 import pygame
 import constants
+import player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
+    pl1 = player.Player(x, y)
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
@@ -15,6 +19,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,"black")
+        pl1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
--- Asteroids/player.py
+++ Asteroids/player.py
@@ -1,9 +1,10 @@
 import circleshape
+import pygame
 from constants import *
 
 class Player(circleshape.CircleShape):
     def __init__(self, x, y):
-        super.__init__(x, y, PLAYER_RADIUS)
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
     def triangle(self):
@@ -13,3 +14,6 @@ class Player(circleshape.CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
